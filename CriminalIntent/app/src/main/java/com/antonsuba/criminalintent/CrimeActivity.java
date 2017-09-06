e655da3c2c62a05989ef7a74343b42b7c782bb43
antonsuba <antonsuba@outlook.com>
2017-09-06 10:40:19 +0800

    Implement crime fragment

--- CriminalIntent/app/src/main/java/com/antonsuba/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/antonsuba/criminalintent/CrimeActivity.java
@@ -3,6 +3,10 @@ package com.antonsuba.criminalintent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author Anton Suba
+ */
+
 public class CrimeActivity extends AppCompatActivity {
 
     @Override
